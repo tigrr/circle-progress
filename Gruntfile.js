1ecb3cef5c675b3c9bc1ec6fea56cb89ffe5553a
Tigran Sargsyan <tigran.sn@gmail.com>
2020-08-22 16:40:57 +0400

    Add UMD support

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,80 @@
 
 'use strict';
 
+
+// Source: UMD (Universal Module Definition)
+// https://github.com/umdjs/umd/blob/master/templates/returnExports.js
+const umdCode = {
+	// Uses Node, AMD or browser globals to create a module.
+	vanilla: {
+		opening:
+`<%= banner %>
+
+'use strict';
+
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define([], factory);
+	} else if (typeof module === 'object' && module.exports) {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like environments that support module.exports,
+		// like Node.
+		module.exports = factory();
+	} else {
+		// Browser globals (root is window)
+		root.CircleProgress = factory();
+  }
+}(typeof self !== 'undefined' ? self : this, function () {
+`,
+		closing:
+`
+	// Just return a value to define the module export.
+	// This example returns an object, but the module
+	// can return a function as the exported value.
+	return CircleProgress;
+}));
+`
+	},
+
+	// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
+	jQuery: {
+		opening:
+`(function (factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['jquery'], factory);
+	} else if (typeof module === 'object' && module.exports) {
+		// Node/CommonJS
+		module.exports = function( root, jQuery ) {
+			if ( jQuery === undefined ) {
+				// require('jQuery') returns a factory that requires window to
+				// build a jQuery instance, we normalize how we use modules
+				// that require this pattern but the window provided is a noop
+				// if it's defined (how jquery works)
+				if ( typeof window !== 'undefined' ) {
+					jQuery = require('jquery');
+				}
+				else {
+					jQuery = require('jquery')(root);
+				}
+			}
+			factory(jQuery);
+			return jQuery;
+		};
+	} else {
+		// Browser globals
+		factory(jQuery);
+	}
+}(function (jQuery) {
+`,
+		closing:
+`
+}));
+`
+	}
+}
+
 module.exports = function (grunt) {
 	// Load all grunt tasks
 	require('load-grunt-tasks')(grunt);
@@ -38,22 +112,32 @@ module.exports = function (grunt) {
 		},
 		concat: {
 			options: {
-				banner: '<%= banner %>\n\n\'use strict\';\n\nconst CircleProgress = (function() {\n',
-				footer: 'return CircleProgress;\n}());',
 				stripBanners: true,
 				process: function(src) {
 					return src.replace('\'use strict\';\n', '');
 				},
 			},
 			vanilla: {
+				options: {
+					banner: umdCode.vanilla.opening,
+					footer: umdCode.vanilla.closing,
+				},
 				src: ['src/innersvg.js', 'src/svgpaper.js', 'src/animator.js', 'src/circle-progress.js'],
 				dest: 'dist/circle-progress.js',
 			},
 			jquery: {
+				options: {
+					banner: umdCode.jQuery.opening,
+					footer: umdCode.jQuery.closing,
+				},
 				src: ['src/innersvg.js', 'src/svgpaper.js', 'src/animator.js', 'src/circle-progress.js', 'lib/jquery.ui.widget.js', 'src/jquery.circle-progress.js'],
 				dest: 'dist/jquery.circle-progress.js',
 			},
 			jqueryBare: {
+				options: {
+					banner: umdCode.jQuery.opening,
+					footer: umdCode.jQuery.closing,
+				},
 				src: ['src/innersvg.js', 'src/svgpaper.js', 'src/animator.js', 'src/circle-progress.js', 'src/jquery.circle-progress.js'],
 				dest: 'dist/jquery.circle-progress.bare.js',
 			}
